56ffcdde6bff11ab8fa6fea25ee5edb551e0e417
Serhii Vidernikov <sv54@mscloud.ua.es>
2023-06-04 16:44:23 +0200

    haciendo login

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+  }
+
+  async login(emailInput: any, passwordInput: any ){
+    console.log(emailInput.value, passwordInput.value);
+    if(emailInput == ''  || passwordInput.value == ''){
+      console.log('Please enter email and password');
+    }
+
+    const requestBody = {
+      email: emailInput.value,
+      password: passwordInput.value
+    };
+
+    this.http.post('https://localhost:3000/medicos/login', requestBody)
+    .subscribe(data => {
+      console.log(data)
+    }, error => {
+      console.log(error)
+    });
+
+
+  }
 
 }
