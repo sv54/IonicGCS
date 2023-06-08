9fcf45f4ccf9f3b8b342505e5fa1e8bf9936b6da
Serhii Vidernikov <sv54@mscloud.ua.es>
2023-06-08 15:20:21 +0200

    Estilo de la pagina de configuracion hecho

--- src/app/configuration-page/configuration-page.page.ts
+++ src/app/configuration-page/configuration-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-configuration-page',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigurationPagePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  guardar(){
+    this.router.navigateByUrl('/main-menu')
+  }
+
+  cancelar(){
+    this.router.navigateByUrl('/main-menu')
   }
 
 }
